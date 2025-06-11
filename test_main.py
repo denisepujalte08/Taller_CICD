7c6a6b64e958debd0369bc1de0acefb65c3cbecb
Denise Pujalte <pujalte64@gmail.com>
2025-06-12 01:33:01 +0200

    a

--- test_main.py
+++ test_main.py
@@ -1,26 +1,15 @@
-from main import es_mayor_de_edad
+from par import es_mayor_de_edad
 
 
-def test_mayor_edad():
+def test_mayor_positivo():
     assert es_mayor_de_edad(20)
 
 
-def test_menor_edad():
-    assert not es_mayor_de_edad(17)
-
-
-def test_limite_exacto():
+def test_exacto():
     assert es_mayor_de_edad(18)
 
 
-def test_limite_inferior():
-    assert not es_mayor_de_edad(0)
+def test_menor():
+    assert not es_mayor_de_edad(17)
 
-
-def test_negativo():
-    assert not es_mayor_de_edad(-5)
-
-
-def test_grande():
-    assert es_mayor_de_edad(120)
 

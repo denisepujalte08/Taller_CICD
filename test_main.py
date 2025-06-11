1ea443579236a0f35a5f63d82ae3971a90006ffe
Denise Pujalte <pujalte64@gmail.com>
2025-06-12 01:29:08 +0200

    agregue espacios

--- test_main.py
+++ test_main.py
@@ -1,19 +1,26 @@
 from main import es_mayor_de_edad
 
+
 def test_mayor_edad():
     assert es_mayor_de_edad(20)
+
 
 def test_menor_edad():
     assert not es_mayor_de_edad(17)
 
+
 def test_limite_exacto():
     assert es_mayor_de_edad(18)
+
 
 def test_limite_inferior():
     assert not es_mayor_de_edad(0)
 
+
 def test_negativo():
     assert not es_mayor_de_edad(-5)
 
+
 def test_grande():
     assert es_mayor_de_edad(120)
+

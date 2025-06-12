ad20f6a6c6c3246af150a750b0c9a211265d88ee
Denise Pujalte <pujalte64@gmail.com>
2025-06-12 14:05:24 +0200

    update tests

--- test_main.py
+++ test_main.py
@@ -11,3 +11,19 @@ def test_exacto():
 
 def test_menor():
     assert not es_mayor_de_edad(17)
+
+
+def test_edad_negativa():
+    assert not es_mayor_de_edad(-1)
+
+
+def test_edad_cero():
+    assert not es_mayor_de_edad(0)
+
+
+def test_float_positivo():
+    assert es_mayor_de_edad(18.5)
+
+
+def test_float_menor():
+    assert not es_mayor_de_edad(17.9)
